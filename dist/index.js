d1e955f8c666f2c66218d518d7f008307fe673ef
Praise Disu <praisedisu@gmail.com>
2022-11-25 13:48:02 +0100

    Log JSON format

--- dist/index.js
+++ dist/index.js
@@ -236,8 +236,8 @@ function compileContract(contractPath, settings) {
                     return [4 /*yield*/, axios_1.default.get('https://binaries.soliditylang.org/bin/list.json')];
                 case 2:
                     compilerList = _b.sent();
-                    console.log('logResult: ', compilerList.data);
-                    releases = compilerList.data.releases;
+                    console.log('logResult: ', JSON.parse(compilerList.data));
+                    releases = JSON.parse(compilerList.data).releases;
                     if (releases[settings.version]) {
                         compilerUrl = releases[settings.version].path;
                         remixCompiler.set('evmVersion', settings.evmVersion);
